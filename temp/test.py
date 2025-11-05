0c98aa27f237bae9b42467adb4f47199a3a37f70
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2025-11-05 12:08:38 +0330

    fix

--- temp/test.py
+++ temp/test.py
@@ -32,7 +32,7 @@ class BaseLogger:
 
 task = Task.init(
     project_name="taha-sama",  # Name of the ClearML project
-    task_name=f"API Training",  # Name of the task
+    task_name=f"API Trainin2g",  # Name of the task
     task_type=Task.TaskTypes.optimizer,  # Type of the task (could also be "training", "testing", etc.)
 )
 logger = BaseLogger()
