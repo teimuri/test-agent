ccf24eab6a0be1e8b3f17cb63cbdf7c97c3f167f
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2025-11-10 15:49:15 +0330

    fix

--- temp/test.py
+++ temp/test.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--n_estimators", type=int, default=100)
 parser.add_argument("--max_depth", type=int,required=True)
 args = parser.parse_args()
-
+print(f"n_estimators: {args.n_estimators}, max_depth: {args.max_depth}")
 # Load dataset
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -25,5 +25,5 @@ clf.fit(X_train, y_train)
 acc = accuracy_score(y_test, clf.predict(X_test))
 
 # Report metric to ClearML
-task.get_logger().report_scalar("accuracy", "test", iteration=0, value=acc)
+# task.get_logger().report_scalar("accuracy", "test", iteration=0, value=acc)
 print(f"Accuracy: {acc:.4f}")
