2a7ad20a6f52bc95e8dab2a70a62c07a24a68205
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2025-11-10 15:46:53 +0330

    fixed

--- temp/test.py
+++ temp/test.py
@@ -7,7 +7,6 @@ import argparse
 
 # Initialize ClearML task
 # task = Task.init(project_name="HPO Example2", task_name="train", reuse_last_task_id=False)
-raise ValueError(102020)
 # Parse hyperparameters
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_estimators", type=int, default=100)
