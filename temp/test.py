190ce0c0bfedc3701b32073f9a12160e791adac1
Mohammad T. Teimuri <35652861+teimuri@users.noreply.github.com>
2025-11-10 16:01:18 +0330

    test

--- temp/test.py
+++ temp/test.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score
 import argparse
 
 # Initialize ClearML task
-task = Task.init(project_name="HPO Example", task_name="train", reuse_last_task_id=False)
+task = Task.init()
 # Parse hyperparameters
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_estimators", type=int, default=100)
